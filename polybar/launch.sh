b23bb49b298db6dda0ea4f052a710177c387f869
Juraj Marcin <juraj@jurajmarcin.com>
2021-08-02 17:00:17 +0200

    Add Polybar config

---
+++ polybar/launch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+if ! type "xrandr"; then
+    polybar --reaload main &
+    exit
+fi
+
+# List Primary and secondary monitors
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+# Launch Polybar on primary monitor and wait one second for system tray to show up
+MONITOR=$PRIMARY polybar --reload main &
+
+# Launch Polybar on other monitors
+for m in $OTHERS; do
+    MONITOR=$m polybar --reload main &
+done
